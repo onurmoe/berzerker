510f5cd54933e025b8e46a9899c82b59b86a12f2
onurmoe <moe52hertz@gmail.com>
2023-10-31 20:25:39 +0100

    Setup

---
+++ index.js
@@ -0,0 +1,10 @@
+const canvas = document.querySelector("canvas"); // picking canvas
+const c = canvas.getContext("2d");
+
+canvas.width = 1024;
+canvas.height = 576;
+
+c.fillStyle = "white";
+c.fillRect(0, 0, canvas.width, canvas.height);
+
+
